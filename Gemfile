a14bdf61721e7db06afa1930fd4f03e850099931
Katry4 <kratos666666@gmail.com>
2013-04-28 15:46:36 +0300

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -1,14 +1,18 @@
 source 'http://rubygems.org'
 
 gem "rails"
-gem 'sqlite3'
 gem 'bcrypt-ruby'
+gem 'sass-rails',   '~> 3.2.5'
+gem 'coffee-rails', '~> 3.2.2'
+gem 'uglifier', '>= 1.2.4'
+gem 'bootstrap-sass', '~> 2.0.3.1'
 
 group :development do
 gem 'rspec-rails', '2.6.1'
 gem 'taps'
 gem 'rvm'
 gem 'annotate'
+gem 'sqlite3'
 end
 
 group :test do
@@ -16,4 +20,5 @@ gem 'rspec-rails', '2.6.1'
 gem 'webrat', '0.7.1'
 gem 'spork'
 gem 'factory_girl_rails'
+gem 'sqlite3'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  match '/signup', :to => 'users#new'
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'#, via: :delete
 
   
 
@@ -21,7 +23,7 @@ SampleApp::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
      resources :users
-
+    resources :sessions, only: [:new, :create, :destroy]
   # Sample resource route with options:
   #   resources :products do
   #     member do
